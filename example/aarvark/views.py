2403f82742ac42b8f698eadf2d921ae66e1b18cf
richtier <contact@richardtier.co.uk>
2020-10-12 17:20:58 +0100

    trigger

--- example/aarvark/views.py
+++ example/aarvark/views.py
@@ -3,6 +3,13 @@ from django.views.generic import View
 from . import models
 
 
+from django.contrib import admin
+
+
+class MyModelAdmin(admin.ModelAdmin):
+	pass
+
+
 class View7(View):
     pass
 
