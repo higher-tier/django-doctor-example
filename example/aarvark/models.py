8e887929b14fc096f47fce539172568819ac70a3
richtier <contact@richardtier.co.uk>
2020-10-15 13:06:08 +0100

    trigger

--- example/aarvark/models.py
+++ example/aarvark/models.py
@@ -76,7 +76,7 @@ class DemoTooManyModelsEleven(models.Model):
 
 
 class NullableCharFieldModel(models.Model):
-    field_one = models.CharField(max_length=5002, null=True)
+    field_one = models.CharField(max_length=5001, null=True)
     field_two = models.CharField(max_length=50)
 
 
