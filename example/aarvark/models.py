63d87b963b4291ca75d1576e236cfe4be382580b
Django Doctor <help@django.doctor>
2021-03-03 08:17:30 +0000

    Fix some django anti-patterns

--- example/aarvark/models.py
+++ example/aarvark/models.py
@@ -113,7 +113,7 @@ class NullableNotBlankFieldModel(models.Model):
 
 
 class UniqueForModel(models.Model):
-    field = models.TextField(null=True, blank=False)
+    field = models.TextField(default='', blank=True)
     unique_for_date = models.DateField(unique_for_date='field')
     unique_for_month = models.DateField(unique_for_month='field')
     unique_for_year = models.DateField(unique_for_year='field')
