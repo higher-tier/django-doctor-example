bacf962c432be91e59016383144b56de47aac1c0
Richard Tier <contact@richardtier.co.uk>
2021-11-10 11:54:12 +0000

    Create helpers.py

---
+++ example/aarvark/helpers.py
@@ -0,0 +1,51 @@
+from unittest import TestCase
+from dataclasses import dataclass
+
+
+try:
+    print()
+except (ValueError, TypeError) as err:
+    pass
+
+
+try:
+    print()
+except ValueError or TypeError:
+    pass
+
+
+async def qux():
+    pass
+
+async def quux():
+    qux()
+
+
+value = 'a'
+
+
+isinstance(value, str) or isinstance(value, int)
+
+
+class Bar:
+    @classmethod
+    def cm():
+        pass
+
+
+value is 1
+
+value != True
+
+
+@dataclass
+class Bar:
+    pass
+
+
+class Bar(TestCase):
+    def foo(self):
+        self.assertEquals("a", "b")
+
+    def bar(self):
+        self.assertTrue("a", "b")
